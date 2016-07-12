14426ae910a2df5f0a254f59162e85889ef2cd67
Hassen Taidirt <htaidirt@gmail.com>
2016-07-12 11:57:54 +0200

    Add property's attributes validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,8 @@ class Property < ApplicationRecord
 
   geocoded_by :address
 
+  validates_presence_of :title, :address, :price, :description
+
   after_validation :geocode, if: :address_changed?
 
 end
